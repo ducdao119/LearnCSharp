f2a02324f0c243d1bf182049184dbbfa68fcd7be
ducdao119 <ducdao11092002@gmail.com>
2025-04-07 14:52:55 +0700

    Learn Class and Struct

---
+++ Day2/ClassVsStruct.cs
@@ -0,0 +1,35 @@
+﻿namespace LearnCSharp.Day2
+{
+    /// <summary>
+    /// Class is reference type
+    /// </summary>
+    public class ClassVsStruct
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Age { get; set; }
+        public ClassVsStruct(string name, string type, int age)
+        {
+            this.Name = name;
+            this.Type = type;
+            this.Age = age;
+        }
+    }
+
+    /// <summary>
+    /// Struct is value type
+    /// </summary>
+    public struct Size
+    {
+        public int Height { get; set; }
+        public int Width { get; set; }
+        public string Type { get; set; }
+
+        public Size(int height, int width, string type)
+        {
+            this.Height = height;
+            this.Width = width;
+            this.Type = type;
+        }
+    }
+}
---
+++ Day2/ProgramDay2.cs
@@ -0,0 +1,41 @@
+﻿namespace LearnCSharp.Day2
+{
+    public static class ProgramDay2
+    {
+        public static void HandleClassesVsStruct()
+        {
+            HandleClasses();
+            HandleStruct();
+        }
+        /// <summary>
+        /// When you assign new value of class, it will change data of relation class
+        /// </summary>
+        public static void HandleClasses()
+        {
+            ClassVsStruct c = new ClassVsStruct("Duc", "Nam", 12);
+            ClassVsStruct b = c;
+            b.Name = "Trung";
+            Console.WriteLine("Class");
+            Console.WriteLine(c.Name);
+            //When you use keyword "defauult". with class the data is "null"
+            ClassVsStruct d = default;
+            //Console.WriteLine($"{d.Age}");
+            Console.WriteLine();
+        }
+        /// <summary>
+        /// When you assign new value of struct, it not effect data of relation struct
+        /// </summary>
+        public static void HandleStruct()
+        {
+            Size s = new Size(10, 20, "Nhua");
+            Size z = s;
+            z.Width = 1000;
+            Console.WriteLine("Struct");
+            Console.WriteLine(s.Width);
+            Console.WriteLine(z.Width);
+            Size w = default;
+            Console.WriteLine($"{w.Width} {w.Height} {w.Type}");
+            Console.WriteLine();
+        }
+    }
+}
