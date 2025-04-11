1a708748e093f38a245f2df61ba5b62826bd9626
Đức Đào Trung <ducdt@kaopiz.com>
2025-04-11 10:37:34 +0700

    init design pattern

---
+++ DesignPattern/Singleton/SingletonSample.cs
@@ -0,0 +1,26 @@
+﻿namespace LearnCSharp.DesignPattern.Singleton
+{
+    internal class SingletonSample
+    {
+        private SingletonSample() { }
+        private static SingletonSample _instance;
+        private static readonly object _lock = new object();
+        public static SingletonSample GetInstance(string value)
+        {
+            if (_instance == null)
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new SingletonSample();
+                        _instance.Value = value;
+                    }
+                }
+            }
+            return _instance;
+        }
+
+        public string Value { get; set; }
+    }
+}
